6adc19d72f750e3b2838f15f3fdd974bb548f252
minseok <imuchgabis@gmail.com>
2023-05-28 16:45:09 +0900

    새로고침 해결

--- back/index.js
+++ back/index.js
@@ -46,7 +46,7 @@ app.use("/image", express.static("./image"));
 app.use(express.static(path.join(__dirname, "../front/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../front/build/index.html", "index.html"));
+  res.sendFile(path.join(__dirname, "../front/build/index.html"));
   // res.send("gkdl");
 });
 
